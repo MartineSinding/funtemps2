22cbf688e78c956d81b1f8118274a85ba41cdee5
martine <martinesi@uia.no>
2023-03-24 14:30:30 +0100

    oppdaterte main importering

--- main.go
+++ main.go
@@ -1,9 +1,10 @@
 package main
 
 import (
-    "flag"
-    "fmt"
-    "is105test/conv"
+	"flag"
+	"fmt"
+
+	"github.com/MartineSinding/funtemps2/conv"
 )
 
 var fahrenheit float64
@@ -12,45 +13,45 @@ var kelvin float64
 var out string
 
 func init() {
-    flag.Float64Var(&fahrenheit, "F", 0.0, "temperatur i grader fahrenheit")
-    flag.Float64Var(&celsius, "C", 0.0, "temperatur i grader celsius")
-    flag.Float64Var(&kelvin, "K", 0.0, "temperatur i grader kelvin")
-    flag.StringVar(&out, "out", "C", "beregne temperatur i C - celsius, F - farhenheit, K- Kelvin")
+	flag.Float64Var(&fahrenheit, "F", 0.0, "temperatur i grader fahrenheit")
+	flag.Float64Var(&celsius, "C", 0.0, "temperatur i grader celsius")
+	flag.Float64Var(&kelvin, "K", 0.0, "temperatur i grader kelvin")
+	flag.StringVar(&out, "out", "C", "beregne temperatur i C - celsius, F - farhenheit, K- Kelvin")
 }
 
 func main() {
-    flag.Parse()
-    if isFlagPassed("out") {
-        if out == "C" && isFlagPassed("F") {
-            fmt.Println(conv.FahrenheitToCelsius(fahrenheit))
-        } else if out == "F" && isFlagPassed("C") {
-            fmt.Println(conv.CelsiusToFahrenheit(celsius))
-        }
-    }
+	flag.Parse()
+	if isFlagPassed("out") {
+		if out == "C" && isFlagPassed("F") {
+			fmt.Println(conv.FahrenheitToCelsius(fahrenheit))
+		} else if out == "F" && isFlagPassed("C") {
+			fmt.Println(conv.CelsiusToFahrenheit(celsius))
+		}
+	}
 
-    if isFlagPassed("out") {
-        if out == "K" && isFlagPassed("C") {
-            fmt.Println(conv.KelvinToCelsius(celsius))
-        } else if out == "C" && isFlagPassed("K") {
-            fmt.Println(conv.CelsiusToKelvin(kelvin))
-        }
-    }
+	if isFlagPassed("out") {
+		if out == "K" && isFlagPassed("C") {
+			fmt.Println(conv.KelvinToCelsius(celsius))
+		} else if out == "C" && isFlagPassed("K") {
+			fmt.Println(conv.CelsiusToKelvin(kelvin))
+		}
+	}
 
-    if isFlagPassed("out") {
-        if out == "K" && isFlagPassed("F") {
-            fmt.Println(conv.FahrenheitToKelvin(kelvin))
-        } else if out == "F" && isFlagPassed("K") {
-            fmt.Println(conv.KelvinToFahrenheit(fahrenheit))
-        }
-    }
+	if isFlagPassed("out") {
+		if out == "K" && isFlagPassed("F") {
+			fmt.Println(conv.FahrenheitToKelvin(kelvin))
+		} else if out == "F" && isFlagPassed("K") {
+			fmt.Println(conv.KelvinToFahrenheit(fahrenheit))
+		}
+	}
 }
 
 func isFlagPassed(name string) bool {
-    found := false
-    flag.Visit(func(f *flag.Flag) {
-        if f.Name == name {
-            found = true
-        }
-    })
-    return found
+	found := false
+	flag.Visit(func(f *flag.Flag) {
+		if f.Name == name {
+			found = true
+		}
+	})
+	return found
 }
